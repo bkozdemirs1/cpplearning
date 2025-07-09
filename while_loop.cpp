46fd8de1143ed95feebded02051e89b0519914a0
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2025-07-09 15:43:27 +0300

    Create while_loop.cpp

---
+++ while_loop.cpp
@@ -0,0 +1,13 @@
+// Prints numbers from 1 to 5 using a while loop
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    while (i <= 5) {
+        cout << i << " ";
+        i++;
+    }
+    cout << endl;
+    return 0;
+}
