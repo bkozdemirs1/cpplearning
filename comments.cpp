413fba2e27873a2a2a51b930bfc3e81bba93ac92
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2025-07-09 14:59:13 +0300

    Create comments.cpp

---
+++ comments.cpp
@@ -0,0 +1,14 @@
+// This is a single-line comment
+
+/*
+    This is a
+    multi-line comment
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Comments are ignored by the compiler!" << endl;
+    return 0;
+}
