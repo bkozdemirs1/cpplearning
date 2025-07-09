91d47cf5709bd2dc147ce642c22abd14331adc90
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2025-07-09 15:00:03 +0300

    Create auto_keyword.cpp

---
+++ auto_keyword.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stl>
+using namespace std;
+
+int main() {
+    auto x = 42;         // int
+    auto y = 3.14;       // double
+    auto name = "Burak"; // const char*
+
+    cout << "x: " << x << ", y: " << y << ", name: " << name << endl;
+    return 0;
+}
