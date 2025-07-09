3025f767271f28eb3f0a9d4348c2d4d2417e65a1
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2025-07-09 14:58:43 +0300

    Create user_input.cpp

---
+++ user_input.cpp
@@ -0,0 +1,17 @@
+// Takes name and age as input and prints them
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Hello " << name << "! You are " << age << " years old." << endl;
+    return 0;
+}
