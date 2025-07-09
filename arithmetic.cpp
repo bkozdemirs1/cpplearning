c4fad5aaa37753a0a9ac9ece524d834633370224
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2025-07-09 15:03:39 +0300

    Create arithmetic.cpp

---
+++ arithmetic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 3;
+
+    cout << "a + b = " << (a + b) << endl;
+    cout << "a - b = " << (a - b) << endl;
+    cout << "a * b = " << (a * b) << endl;
+    cout << "a / b = " << (a / b) << endl;
+    cout << "a % b = " << (a % b) << endl; 
+
+    return 0;
+}
+
+
+//This percentage "%" sign represents the remainder of a number from another number and can be confusing for those who don't know.
+//And if you set your numbers as integers when you try to divide a even number to odd number it only gives you the integer but if you want to know the other languages--
+//you can set as float :D
+
