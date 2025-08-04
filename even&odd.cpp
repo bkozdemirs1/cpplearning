87766ee988f74a6e184bcea047c3920604b2527e
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2025-08-04 15:32:05 +0300

    Update and rename 9-even_or_odd.cpp to even&odd.cpp

---
+++ even&odd.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number,i;
+    
+    cout<<"Please enter a number: ";
+    
+    cin>>number;
+    
+    for(i=1;i<=number;i++){
+        
+     if(i%2==0)
+         
+         cout<<"Even numbers: "<<i<<endl;
+      
+         else
+        
+        cout<<"Odd numbers:  "<<i<<endl;
+    }
+    }    
+        
+        
+   
+    
+    
+    
+    
