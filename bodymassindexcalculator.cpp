bd76bb2821c04a567da09799b9fc59236481e735
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2025-08-10 20:33:52 +0300

    Create bodymassindexcalculator.cpp

---
+++ bodymassindexcalculator.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int bmi;
+    
+    int weight;
+
+    float height;
+
+    cout<<"Please enter your weight(kg) and your height (m) : ";
+
+    cin>>weight>>height;
+
+    bmi=(weight)/(height*height);
+
+    if(bmi<18.5){
+    
+    cout<<"Skinny"<<endl;
+    }
+    
+    else if (bmi>18.5 && bmi<24.9){
+    
+        cout<<"Normal"<<endl;
+    }
+    
+    else if(bmi>25 && bmi<29.9){
+    
+        cout<<"Overweight"<<endl;
+    }
+
+   else if(bmi>30){
+    
+       cout<<"Obese"<<endl;
+   }
+    
+    return 0;
+}
