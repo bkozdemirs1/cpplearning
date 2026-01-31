3bcfe875118f591fb1b0e575e8a9293924f06c9a
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2026-01-31 03:31:05 +0300

    Update 4-auto_keyword.cpp

--- 4-auto_keyword.cpp
+++ 4-auto_keyword.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     auto x = 42;         // int
     auto y = 3.14;       // double
-    auto name = "Burak"; // const char*
+    auto name = "Burak"; // char
 
     cout << "x: " << x << ", y: " << y << ", name: " << name << endl;
     return 0;
